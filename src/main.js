c49393496e53fa89f4af0372a9cbf2b6a2758b56
hasnaa-ux <hasnaa3193378@gmail.com>
2022-11-02 15:31:36 +0200

    c1

--- src/main.js
+++ src/main.js
@@ -1,11 +1,8 @@
-import { createApp } from 'vue';
-import { createRouter , createWebHistory  } from "vue-router";
 import App from './App.vue';
 
-const router=createRouter({
-    history: createWebHistory(),
-    routes:[],
-});
+import { createApp } from 'vue';
+
+
 
 /*
 import Vue from 'vue';
